5fd6108ed35ea73c9b18e365ad96994e0296710a
SaidinUK <icestarcsgo@gmail.com>
2017-02-19 17:27:07 +0000

    Homepage updates

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -48,7 +48,7 @@
     	<div class="container">
     		<div class="row">
     			<div class="col-md-10">
-    				<h1><span class="glyphicon glyphicon-tower" aria-hidden="true"></span> Clanbase <small>A remake of the classic league</small></h1>
+    				<h1><span class="glyphicon glyphicon-tower" aria-hidden="true"></span> Clanbase <small>Development edition</small></h1>
     			</div>
     			<div class="col-md-2">
     				<div class="dropdown create">
@@ -80,18 +80,159 @@
     		<div class="row">
 	    		<div class="col-md-3">
 	    			<div class="list-group">
-						<a href="#" class="list-group-item active">
+						<a href="#" class="list-group-item active main-color-bg title">
 							Navigation
 						</a>
-						<a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-						<a href="#" class="list-group-item">Morbi leo risus</a>
-						<a href="#" class="list-group-item">Porta ac consectetur ac</a>
-						<a href="#" class="list-group-item">Vestibulum at eros</a>
+						<a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Get the client</a>
+						<a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Become a premium member</a>
+						<a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Create a team</a>
+						<a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Join a Cup <span class="badge">15</span></a>
+						<a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Join a Ladder <span class="badge">56</span></a>
+						<a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Latest bans <span class="badge">1,905</span></a>
+						<a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Leaderboards</a>
+						<a href="#" class="list-group-item"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Downloads</a>
+					</div>
+					<div class="well well-progress-bars">
+						<h4>Site completion</h4>
+						<div class="progress">
+						  <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+						    20%
+						  </div>
+						</div>
+						<h4>Server costs</h4>
+						<div class="progress">
+						  <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+						    80%
+						  </div>
+						</div>
 					</div>
 	    		</div>
+
+	    		<!-- Main body of content -->
+
 	    		<div class="col-md-9">
 
+	    			<!-- Featured items -->
+
+	    			<div class="panel panel-default">
+					  	<div class="panel-heading main-color-bg">
+					    	<h3 class="panel-title">Featured items</h3>
+					 	 </div>
+					 	 <div class="panel-body">
+
+						    <div class="col-md-3">
+						    	<div class="well featured-items">
+						    		<h2><span class="glyphicon glyphicon-screenshot" aria-hidden="true"></span> 5,000</h2>
+						    		<h4>Headshots today</h4>
+						    	</div>
+						    </div>
+
+						    <div class="col-md-3">
+						    	<div class="well featured-items">
+						    		<h2><span class="glyphicon glyphicon-screenshot" aria-hidden="true"></span> 5,000</h2>
+						    		<h4>Headshots today</h4>
+						    	</div>
+						    </div>
+
+						    <div class="col-md-3">
+						    	<div class="well featured-items">
+						    		<h2><span class="glyphicon glyphicon-screenshot" aria-hidden="true"></span> 5,000</h2>
+						    		<h4>Headshots today</h4>
+						    	</div>
+						    </div>
+
+						    <div class="col-md-3">
+						    	<div class="well featured-items">
+						    		<h2><span class="glyphicon glyphicon-screenshot" aria-hidden="true"></span> 5,000</h2>
+						    		<h4>Headshots today</h4>
+						    	</div>
+						    </div>
+
+					 	 </div>
+					</div>
+
+					<!-- Ladders -->
+
+					<div class="panel panel-default">
+						<div class="panel-heading main-color-bg">
+							<h3 class="panel-title">Ladder: Top 5</h3>
+						</div>
+						<div class="panel-body">
+							<table class="table table-striped table-hover centre-text">
+								<tr>
+									<th>RANK</th>
+									<th>RATING</th>
+									<th>TEAM NAME</th>
+									<th>COUNTRY</th>
+									<th>WIN</th>
+									<th>DRAW</th>
+									<th>LOSE</th>
+									<th>STREAK</th>
+									<th>CHALLENGE</th>
+								</tr>
+								<tr>
+									<th>1</th>
+									<th>2000</th>
+									<th>Team Saidin</th>
+									<th>UK</th>
+									<th class="font-green">25</th>
+									<th class="font-orange">1</th>
+									<th class="font-red">0</th>
+									<th class="font-green">24W</th>
+									<th><button type="button" class="btn btn-danger">Challenge</button></th>
+								</tr>
+								<tr class="row-offset">
+									<th>2</th>
+									<th>1900</th>
+									<th>Team Apex</th>
+									<th>FR</th>
+									<th class="font-green">23</th>
+									<th class="font-orange">3</th>
+									<th class="font-red">4</th>
+									<th class="font-green">18W</th>
+									<th><button type="button" class="btn btn-danger">Challenge</button></th>
+								</tr>
+								<tr>
+									<th>2</th>
+									<th>1700</th>
+									<th>Lion's Roar</th>
+									<th>DE</th>
+									<th class="font-green">18</th>
+									<th class="font-orange">3</th>
+									<th class="font-red">9</th>
+									<th class="font-green">8W</th>
+									<th><button type="button" class="btn btn-danger">Challenge</button></th>
+								</tr>
+								<tr class="row-offset">
+									<th>4</th>
+									<th>1500</th>
+									<th>Plebs United</th>
+									<th>RU</th>
+									<th class="font-green">12</th>
+									<th class="font-orange">1</th>
+									<th class="font-red">17</th>
+									<th class="font-green">2W</th>
+									<th><button type="button" class="btn btn-danger">Challenge</button></th>
+								</tr>
+								<tr>
+									<th>5</th>
+									<th>1100</th>
+									<th>Team Aura</th>
+									<th>UK</th>
+									<th class="font-green">7</th>
+									<th class="font-orange">3</th>
+									<th class="font-red">20</th>
+									<th class="font-red">2L</th>
+									<th><button type="button" class="btn btn-danger">Challenge</button></th>
+								</tr>
+							</table>
+						</div>
+					</div>
+
 	    		</div>
+
+	    		<!-- End of main body of content -->
+
     		</div>
     	</div>
    	</section>
