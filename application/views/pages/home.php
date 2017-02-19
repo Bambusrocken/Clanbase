65601a7ed01466cd09b9c4eb254c7513e82fbb79
SaidinUK <icestarcsgo@gmail.com>
2017-02-19 02:19:11 +0000

    Removed unnecessary Bootstrap CSS files

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -24,13 +24,18 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">Clanbase</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#">Ladders</a></li>
+            <li><a href="#">Cups</a></li>
+            <li><a href="#">Support</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Welcome, $user</a></li>
+            <li><a href="#">Logout</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
